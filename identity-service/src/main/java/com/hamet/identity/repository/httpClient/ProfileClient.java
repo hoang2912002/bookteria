d1db4ce092deb3262efe5e4a711f7c3e3397f66c
hoang2912002 <hoang2912002@gmail.com>
2025-12-25 15:36:12 +0700

    Configuration feign chèn token vào header khi gọi api bằng openfeign để service consumer check authorization

--- identity-service/src/main/java/com/hamet/identity/repository/httpClient/ProfileClient.java
+++ identity-service/src/main/java/com/hamet/identity/repository/httpClient/ProfileClient.java
@@ -7,8 +7,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import com.hamet.identity.dto.request.ProfileCreateRequest;
 import com.hamet.identity.dto.response.UserProfileResponse;
+import com.hamet.identity.service.AuthenticationRequestInterceptor;
 
-@FeignClient(name = "profile-service", url = "${app.service.profile-service}")
+@FeignClient(name = "profile-service", url = "${app.service.profile-service}",
+    configuration = {AuthenticationRequestInterceptor.class}
+    // Thêm token vào header
+)
 public interface ProfileClient {
     @PostMapping(value = "/internal/users", produces = MediaType.APPLICATION_JSON_VALUE)
     UserProfileResponse createProfile(@RequestBody ProfileCreateRequest createRequest);
